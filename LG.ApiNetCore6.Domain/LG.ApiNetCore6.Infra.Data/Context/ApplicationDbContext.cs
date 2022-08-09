0b4f3138f0264f91cc48e29ede952605e5200c57
LeandroLeeh32 <leandroleeh32@gmail.com>
2022-08-09 18:55:34 -0300

    5 - Criando nosso DBcontext mapeando a classe pessoa

---
+++ LG.ApiNetCore6.Domain/LG.ApiNetCore6.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using LG.ApiNetCore6.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LG.ApiNetCore6.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {}
+        public DbSet<Pessoa> MyProperty { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+
+}
---
+++ LG.ApiNetCore6.Domain/LG.ApiNetCore6.Infra.Data/Maps/PersonMap.cs
@@ -0,0 +1,29 @@
+﻿using LG.ApiNetCore6.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LG.ApiNetCore6.Infra.Data.Maps
+{
+    public class PersonMap : IEntityTypeConfiguration<Pessoa>
+    {
+        public void Configure(EntityTypeBuilder<Pessoa> builder)
+        {
+            builder.ToTable("TB_PESSOA");
+            builder.HasKey(x => x.Id);
+            
+            builder.Property(x => x.Id).HasColumnName("IDPESSOA").UseIdentityColumn();
+            builder.Property(x => x.Nome).HasColumnName("NOME").UseIdentityColumn();
+            builder.Property(x => x.Celular).HasColumnName("CELULAR").UseIdentityColumn();
+
+            builder.HasMany(c => c.Compra).WithOne(p => p.Pessoa).HasForeignKey(c => c.PessoaId);
+
+
+
+        }
+    }
+}
