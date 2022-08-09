373ed4f7a30d4efe431fb8d5b128b387cacb7226
LeandroLeeh32 <leandroleeh32@gmail.com>
2022-08-09 20:51:00 -0300

    6-Mapeando a classe de produto e compra

---
+++ LG.ApiNetCore6.Domain/LG.ApiNetCore6.Infra.Data/Maps/CompraMap.cs
@@ -0,0 +1,33 @@
+﻿using LG.ApiNetCore6.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LG.ApiNetCore6.Infra.Data.Maps
+{
+    public class CompraMap : IEntityTypeConfiguration<Compra>
+    {
+        public void Configure(EntityTypeBuilder<Compra> builder)
+        {
+            builder.ToTable("TB_COMPRA");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(X => X.Id).HasColumnName("IDCOMPRA").UseIdentityColumn();
+
+            builder.Property(x => x.ProdutoId).HasColumnName("IDPRODUTO");
+            builder.Property(x => x.PessoaId).HasColumnName("IDPESSOA");
+            builder.Property(X => X.Data).HasColumnName("DATA");
+
+            builder.HasOne(x => x.Pessoa).WithMany(x => x.Compra);
+            builder.HasOne(x =>x.Produto).WithMany(x => x.Compra);
+
+ 
+
+
+        }
+    }
+}
---
+++ LG.ApiNetCore6.Domain/LG.ApiNetCore6.Infra.Data/Maps/ProdutoMap.cs
@@ -0,0 +1,30 @@
+﻿using LG.ApiNetCore6.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LG.ApiNetCore6.Infra.Data.Maps
+{
+    public class ProdutoMap : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.ToTable("TB_PRODUTO");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasColumnName("IDPRODUTO").UseIdentityColumn();
+
+            builder.Property(x => x.Descricao).HasColumnName("DESCRICAO");
+            builder.Property(x => x.Codigo).HasColumnName("CODIGO");
+            builder.Property(x => x.Preco).HasColumnName("PRECO");
+
+            builder.HasMany(x=>x.Compra).WithOne(p=>p.Produto).HasForeignKey(x=>x.ProdutoId);
+
+         
+        }
+    }
+}
